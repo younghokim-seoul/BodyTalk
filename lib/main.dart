be5601260c59e3c4de7ea67516e588748dd5ff17
cmyh <ksemfn@gmail.com>
2026-03-04 22:32:14 +0900

    앱 부트스트랩 안정성 개선 및 의존성 업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,46 @@
+import 'dart:async';
+
 import 'package:bodytalk/di/injection_container.dart' as di;
 import 'package:bodytalk/presentation/splash/splash_screen.dart';
-import 'package:bodytalk/presentation/util/app_colors.dart';
 import 'package:bodytalk/presentation/util/app_size.dart';
 import 'package:flutter/material.dart';
-
-import 'presentation/detail/detail_screen.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await di.init();
-  runApp(const MyApp());
+  FlutterError.onError = _handleFlutterError;
+
+  await runZonedGuarded(
+    () async {
+      await di.init();
+      _initLoadingIndicator();
+      runApp(const MyApp());
+    },
+    _logUnhandledError,
+  );
+}
+
+void _initLoadingIndicator() {
+  EasyLoading.instance
+    ..indicatorType = EasyLoadingIndicatorType.ring
+    ..loadingStyle = EasyLoadingStyle.custom
+    ..backgroundColor = Colors.transparent
+    ..boxShadow = []
+    ..indicatorColor = Colors.white
+    ..maskType = EasyLoadingMaskType.black
+    ..maskColor = Colors.transparent
+    ..textColor = Colors.white
+    ..dismissOnTap = false;
+}
+
+void _handleFlutterError(FlutterErrorDetails details) {
+  FlutterError.presentError(details);
+  _logUnhandledError(details.exception, details.stack ?? StackTrace.empty);
+}
+
+void _logUnhandledError(Object error, StackTrace stackTrace) {
+  debugPrint('Unhandled error: $error');
+  debugPrintStack(stackTrace: stackTrace);
 }
 
 class MyApp extends StatelessWidget {
@@ -17,13 +48,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    AppSize.init(context);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.themeSeed),
+      title: 'BodyTalk',
+      themeMode: ThemeMode.light,
+      theme: ThemeData.light(),
+      home: const SplashScreen(),
+      builder: EasyLoading.init(
+        builder: (context, child) {
+          AppSize.init(context);
+          return child!;
+        },
       ),
-      home: const DetailScreen(),
     );
   }
 }
