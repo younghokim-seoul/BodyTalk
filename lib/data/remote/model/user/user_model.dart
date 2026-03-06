1dd527e95b774f34ee0f93a7f641e4a5c1cade47
cmyh91 <ksemfn@gmail.com>
2026-03-06 17:55:13 +0900

    fix(model): UserModel을 freezed 규칙에 맞게 abstract로 변경

--- lib/data/remote/model/user/user_model.dart
+++ lib/data/remote/model/user/user_model.dart
@@ -4,7 +4,7 @@ part 'user_model.freezed.dart';
 part 'user_model.g.dart';
 
 @freezed
-class UserModel with _$UserModel {
+abstract class UserModel with _$UserModel {
   const factory UserModel({
     @JsonKey(name: 'user_id') required String userId,
     @JsonKey(name: 'user_nm') required String userNm,
